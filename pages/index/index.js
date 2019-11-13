f7f8c47f7f59e8d62cc66a998e544604a2797f28
sdlas <2749948168@qq.com>
2019-11-13 23:11:19 +0800

    完成了页面逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,37 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    getinfo:false,//是否测量过身体数据
+    setfood:false,//是否定制过套餐
+    todaylist:[1,2,3],//今日食谱
+    updatetime:'11/8',
+    foodlist:[1,2,3,4,5],//首页推荐套餐图
+    newrecom:'奥尔良鸡胸肉套餐',
+    windowWitdh:[],
+    windowHeight:[]
   },
   //事件处理函数
-  bindViewTap: function() {
+  fillindata:function(){//跳转到填写个人身体信息页面
+    var that = this
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../fillindata/fillindata',
+    }) 
+  },
+  recipelist:function(){
+    var that = this
+    wx.navigateTo({
+      url:'../recipelist/recipelist',
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    var that = this
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          windowHeight: res.windowHeight,
+          windowWidth: res.windowWidth,
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      },
     })
-  }
+  },
 })
