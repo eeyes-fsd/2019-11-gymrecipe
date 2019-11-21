0c3bca4dd76c49daa9519498cc80030808bd0167
mhq1065 <mhq1065@qq.com>
2019-11-21 22:02:15 +0800

    补充js

--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,93 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+// 地址
+const host = 'https://app0.eeyes.xyz/api'
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+// 封装requests
+const requestPromise = (method, url, data = "", token = "") => {
+  if (token === "") {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        method: method,
+        url: `${host}${url}`,
+        data: data,
+        header: {
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+        success: res => resolve(res),
+        fail: (res) => reject(res)
+      })
+    })
+  } else {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        method: method,
+        url: `${host}${url}`,
+        data: data,
+        header: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'Authorization': `Bearer ${token}`
+        },
+        success: res => resolve(res),
+        fail: (res) => reject(res)
+      })
+    })
+  }
 }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+// 登录
+const login = async(codes, phone) => {
+  let data = {
+    "code": codes, //code
+    "phone": phone //手机号码
+  }
+  let response = await requestPromise("POST", "/authorizations/weapp", data)
+  return response;
+}
+
+// 刷新token
+const refreshToken = async() => {
+  let token = wx.getStorageSync("access_token")
+  let response = await requestPromise("PUT", "/authorizations/current")
+  wx.setStorageSync("access_token", response.data.access_token)
+  wx.setStorageSync("expires_in", response.data.expires_in)
+  return response.data.access_token
+}
+
+//获取token
+const getToken = async() => {
+  let response = wx.getStorageSync("access_token")
+  if (response) {
+    response = await refreshToken()
+  }
+  return response
+}
+
+//获取用户数据
+const getUser = async() => {
+  let token = getToken()
+  let response = await requestPromise("GET", "/user", "", token)
+  return response
+}
+
+//删除用户
+const delUser = async() => {
+  let token = getToken()
+  let response = await requestPromise("DELETE", "/user", "", token)
+  return response
+}
+
+//更新用户数据
+const upDateUser = async(phone, captcha) => {
+  let data = {
+    "phone": phone,
+    "captcha": captcha
+  }
+  let token = getToken()
+  let response = await requestPromise("DELETE", "/user", data, token)
+  return response
 }
 
 module.exports = {
-  formatTime: formatTime
+  requestPromise,
+  login,
+  getToken
 }
