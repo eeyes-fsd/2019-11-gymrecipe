e7bc8b073c77f10ef4e82151a12220d897aa5f6e
sdlas <2749948168@qq.com>
2019-11-29 19:43:50 +0800

    删除了图片文件，但图片路径仍保存了，会报错

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,25 +4,14 @@ const app = getApp()
 import api from "../../utils/util.js"
 Page({
   data: {
-<<<<<<< HEAD
     getinfo:false,//是否测量过身体数据
     setfood:false,//是否定制过套餐
-    todaylist:[1,2,3],//今日食谱
+    todaylist:[{id:1,imagesrc:"/images/dailytwo.png",content:"早餐-牛油果奶昔"},{id:2,imagesrc:"/images/dailythree.png",content:"午餐-牛油果意面"},{id:3,imagesrc:"/images/dailyone.png",content:"晚餐-牛油果沙拉"}],//今日食谱
     updatetime:'11/8',
     foodlist: [{id:1,imagesrc:"/images/meat1.png"}, {id:2,imagesrc:"/images/meat.png"}],//首页推荐套餐图
     newrecom:'奥尔良鸡胸肉套餐',
     windowWitdh:[],
     windowHeight:[]
-=======
-    getinfo: false, //是否测量过身体数据
-    setfood: false, //是否定制过套餐
-    todaylist: [1, 2, 3], //今日食谱
-    updatetime: '11/8',
-    foodlist: [1, 2, 3, 4, 5], //首页推荐套餐图
-    newrecom: '奥尔良鸡胸肉套餐',
-    windowWitdh: [],
-    windowHeight: []
->>>>>>> 0c3bca4dd76c49daa9519498cc80030808bd0167
   },
   //事件处理函数
   fillindata: function() { //跳转到填写个人身体信息页面
