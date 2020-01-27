a4c43425b3afc470c62dbf080561690c4b6fa4e8
sdlas <2749948169@qq.com>
2020-01-27 12:33:16 +0800

    recipebuy

--- pages/recipelist/recipelist.js
+++ pages/recipelist/recipelist.js
@@ -24,15 +24,21 @@ Page({
     currenttab: "breakfast", //早餐午餐晚餐切换
 
     //recipelist
+    onedayprice:0,//一日配餐价格
+    threedayprice:0,//三日配餐价格
+    allprice:0,//食谱总价
     boolmyrecipelist:false,//判断我的购买是否为空
     fooddetail: false, //是否显示图片详情
-    methodid: 1, //配餐定制方式
+    methodid: 2, //配餐定制方式
     showwindow: false, //是否显示配置订餐窗口
     perchased: 1,
     //全部套餐
     recipelist: [],
     //已购套餐
     myrecipelist: [],
+    copyrecipelist:[],//recipelist的拷贝，用于显示购买浮窗中两个额外配餐
+    choosing:false,//判断是否选择了该套餐
+    choosingrecipe:[],//被选中的食谱
   },
 
   //早餐午餐晚餐切换
@@ -61,13 +67,15 @@ Page({
   },
 
   //recepelist
-  getposition: function(e) { //获取当前sroll滚动位置
+  //获取当前sroll滚动位置
+  getposition: function(e) { 
     var that = this
     that.setData({
       testx: (e.detail.scrollLeft) * that.data.trans
     })
   },
-  methodbar: function(e) { //餐品选择
+  //餐品选择
+  methodbar: function(e) { 
     var that = this
     console.log(e)
     that.setData({
@@ -81,6 +89,68 @@ Page({
       showwindow: true,
       payid:e.currentTarget.dataset.id
     })
+
+    //深拷贝recipelist,三日套餐选择的食谱列表，去掉了当前食谱以及已购食谱
+    var copy = JSON.parse(JSON.stringify(that.data.recipelist))
+    //将当前食谱从列表中删去
+    for(var p in copy){
+      if (parseInt(e.currentTarget.dataset.id)===copy[p].id){
+        //设置一日配餐的价格
+        that.setData({
+          choosingrecipe:copy[p],
+          onedayprice:copy[p].price,
+          threedayprice: copy[p].price
+        })
+        copy.splice(p,1);
+        break;
+      }
+    }
+    //将已购食谱从列表中删去
+    for(var p in that.data.myrecipelist){
+      for(var q in copy){
+        if(that.data.myrecipelist[p].id===copy[q].id){
+          copy[q].splice(q,1);
+          break;
+        }
+      }
+    }
+    //为每一个数组成员添加新的键值对判断是否选中了该食谱
+    for(var i=0;i<copy.length;i++){
+      copy[i].choosing = false
+    }
+    that.setData({
+      copyrecipelist:copy
+    })
+  },
+  //三日配餐的选择
+  buychoosing:function(e){
+    var that = this
+    var id = parseInt(e.currentTarget.dataset.id)
+    for(var p in that.data.copyrecipelist){
+      if(id===that.data.copyrecipelist[p].id){
+        if (that.data.copyrecipelist[p].choosing)
+        { 
+          that.data.copyrecipelist[p].choosing = false;
+          //修改价格
+          that.setData({
+            threedayprice:that.data.threedayprice-that.data.copyrecipelist[p].price
+          })
+        }
+        else{ 
+          that.data.copyrecipelist[p].choosing = true;
+          //修改价格
+          that.setData({
+            threedayprice: that.data.threedayprice + that.data.copyrecipelist[p].price
+          })
+        }
+        break;
+      }
+    }
+    //自己赋值给自己以刷新页面显示
+    that.setData({
+      copyrecipelist:that.data.copyrecipelist
+    })
+    //
   },
   //切换已购买和全部
   nav: function(e) {
@@ -156,7 +226,19 @@ Page({
     that.setData({
       showwindow: false
     })
-    await payment.pay(that.data.payid)
+    var idarray = new Array()
+    if (parseInt(that.data.methodid) === 1) idarray.push(that.data.payid)
+    if (parseInt(that.data.methodid)===2){
+      idarray.push(that.data.payid)
+      for(var p in that.data.copyrecipelist){
+        if(that.data.copyrecipelist[p].choosing){
+          idarray.push(that.data.copyrecipelist[p].id)
+        }
+      }
+    }
+    if (parseInt(that.data.methodid)===3) idarray = 0
+    await payment.pay(idarray)
+    console.log(idarray)
   },
   /**
    * 生命周期函数--监听页面加载
@@ -169,7 +251,7 @@ Page({
       recipelist: response.data.data
     })
     let response2 = await api.boughtRecipes()
-    if(response2.status==200){
+    if(response2.status===200){
       that.setData({
         myrecipelist: response2.data.data,
         boolmyrecipelist:true
@@ -202,7 +284,14 @@ Page({
         })
       }
     })
-
+    //计算出总的价格
+    var allprice=0
+    for(var i=0;i<that.data.recipelist.length;i++){
+      allprice = allprice + that.data.recipelist[i].price
+    }
+    that.setData({
+      allprice:allprice
+    })
   },
 
   /**
